a6170d2b6927c60d15c7061930af20fcf343a460
s-beats <sakashin10291029@gmail.com>
2022-11-03 15:18:06 +0900

    delete and create user

--- user.sh
+++ user.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-useradd --create-home --shell /bin/bash beats
+USER=${1}
+
+echo "delete ${USER}"
+userdel beats
+
+echo "create ${USER}"
+useradd --create-home --shell /bin/bash --password PASSWORD beats
